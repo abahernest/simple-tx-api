01f2ad919ecbc48d2b8c0e293a8ddc9d4f511521
abahernest <abahernesto@gmail.com>
2022-03-30 12:09:30 +0100

    preprare for deployment to heroku

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 import knex from "knex";
 
 const connectionConfig = {
-    host : '127.0.0.1',
-    port : 3306,
-    user : 'apple',
-    password : 'kylegenius',
-    database : 'lendsqr_dev'
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
 }
 const db = knex({
   client: 'mysql2',
--- knexfile.cjs
+++ knexfile.cjs
@@ -4,22 +4,22 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
   development: {
-    client: 'mysql2',
+    client: "mysql2",
     connection: {
-      database: 'lendsqr_dev',
-      user:     'apple',
-      password: 'kylegenius'
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'migrations',
-      directory: './migrations',
-    }
+      tableName: "migrations",
+      directory: "./migrations",
+    },
   },
-
 };
